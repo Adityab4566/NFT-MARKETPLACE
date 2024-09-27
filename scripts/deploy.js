c7d2bf5792da279473d164b46b6d9559ba333bf1
Aditya <157621878+Adityab4566@users.noreply.github.com>
2024-09-27 08:02:45 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+const hre = require('hardhat')
+
+async function sleep(ms){
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function main(){
+    const collection = await hre.ethers.deployContract("NftCollection")
+    collection.waitForDeployment()
+    console.log(`NFT Collection deployed at : ${collection.target}`);
+    const marketPlace = await hre.ethers.deployContract("NFTMarketplace",[collection.target])
+    marketPlace.waitForDeployment()
+
+    await sleep(30 * 1000)
+
+    await hre.run('verify:verify',{
+        address : collection.target,
+        constructorArguments : []
+    })
+
+    await hre.run('verify:verify',{
+        address : marketPlace.target,
+        constructorArguments : [collection.target]
+    })
+}
+
+main().catch((err)=>{
+    console.error(err);
+})
